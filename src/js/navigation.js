1fd337259febe2d1d693966aaab403819bd0d55c
TheKureq <xkurekx@gmail.com>
2023-02-25 00:01:45 +0100

    navigation javaScript logic, warmup

---
+++ src/js/navigation.js
@@ -0,0 +1,23 @@
+let nav = document.querySelector('.nav')
+let trigger = document.querySelector('.nav__trigger')
+let icon = document.querySelector('.fa-solid')
+
+const triggerHandle = () => {
+	nav.classList.toggle('show')
+
+	if (nav.classList.contains('show')) {
+		trigger.style.transform = 'translate(50%, -50%) rotate(180deg)'
+		icon.style.marginLeft = '0rem'
+		icon.style.marginRight = '4rem'
+	} else {
+		trigger.style.transform = 'translate(50%, -50%) rotate(0deg)'
+		icon.style.marginLeft = '5rem'
+		icon.style.marginRight = '0rem'
+	}
+}
+
+const mainNavJs = () => {
+	trigger.addEventListener('click', triggerHandle)
+}
+
+document.addEventListener('DOMContentLoaded', mainNavJs)
